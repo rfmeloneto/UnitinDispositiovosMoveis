d49811da0471bee250deb0a71f8f2a3fe87485f8
Raimundo Melo <raimundomelo@mpto.mp.br>
2024-12-20 11:48:06 -0300

    banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:primeiro_app/exemplo_aspect_ratio.dart';
-import 'package:primeiro_app/exemplo_expanded.dart';
-import 'package:primeiro_app/exemplo_flexible.dart';
-import 'package:primeiro_app/exemplo_single_child_scroll_view.dart';
-import 'package:primeiro_app/exemplo_wrap.dart';
-import 'package:primeiro_app/media_query_vs_layout_builder.dart';
-import 'package:primeiro_app/page_with_media_query.dart';
+import 'package:primeiro_app/presenter/to_do_page.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -20,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        home: ExemploSingleChildScrollView(),
+        home: TodoListScreen(),
       );
   }
   
